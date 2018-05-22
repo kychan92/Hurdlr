57ca7d30fe25a27992e431b2f5c56ccb97a3bd35
Kai Chan <kchan@nl.ibm.com>
2018-05-22 19:38:07 +0200

    Remove old backups

--- BCK/4/utils/Uptime.js
+++
@@ -1,7 +0,0 @@
-let date = new Date();
-
-let prepend = (val) => {
-    return val < 10 ? '0' + val : val;
-};
-
-module.exports = date.toLocaleDateString('nl-NL') + ` ${prepend(date.getHours())}:${prepend(date.getMinutes())}:${prepend(date.getSeconds())}`;
